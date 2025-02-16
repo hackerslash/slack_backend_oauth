58eace9fc974661365cc29ff89a35f6432df1bb4
hackerslash <afridijnv19@gmail.com>
2025-02-16 19:38:17 +0530

    feat: add conversation sync to mongo

--- models/BotInstallation.js
+++ models/BotInstallation.js
@@ -1,8 +1,15 @@
 // models/BotInstallation.js
 const mongoose = require('mongoose');
 
+const conversationSchema = new mongoose.Schema({
+  message: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
 const botInstallationSchema = new mongoose.Schema({
   teamId: { type: String, required: true, unique: true },
   botToken: { type: String, required: true },
+  conversations: [conversationSchema],
 });
+
 module.exports = mongoose.model('BotInstallation', botInstallationSchema);
